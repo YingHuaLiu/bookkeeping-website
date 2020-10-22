007d56067a5fa68b920670182b94478fde7d04eb
YingHuaLiu <1042413535@qq.com>
2020-10-22 10:19:03 +0800

    deploy

---
+++ precache-manifest.d3feacbc52c73a0d3535e66e241e923e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbdeac12dae65a917fcdf62fe21789df",
+    "url": "/bookkeeping-website/index.html"
+  },
+  {
+    "revision": "3b97f33eccb7b79c7681",
+    "url": "/bookkeeping-website/static/css/main.42c18d61.chunk.css"
+  },
+  {
+    "revision": "17e7e4bd317f8db4537a",
+    "url": "/bookkeeping-website/static/js/2.0db0d9c9.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/bookkeeping-website/static/js/2.0db0d9c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b97f33eccb7b79c7681",
+    "url": "/bookkeeping-website/static/js/main.cc4a42fa.chunk.js"
+  },
+  {
+    "revision": "d9a87aa8f0775bfabaf6",
+    "url": "/bookkeeping-website/static/js/runtime-main.a06ddb3f.js"
+  }
+]);
